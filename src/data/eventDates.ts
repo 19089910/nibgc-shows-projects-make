8e73560ccdcbd15b83c5998f232936fbc70b7d67
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2025-04-12 20:50:22 -0400

    feat: add discription in index.html

--- src/data/eventDates.ts
+++ src/data/eventDates.ts
@@ -1,4 +1,4 @@
 export const eventDates = [
-    "13 Abril", "16 Abril", "17 Abril", "18 Abril", "19 Abril", "20 Abril", "21 Abril"
+    "12 Abril","13 Abril", "16 Abril", "17 Abril", "18 Abril", "19 Abril", "20 Abril", "21 Abril"
   ];
   
