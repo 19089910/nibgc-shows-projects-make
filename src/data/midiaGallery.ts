16e6c7f02df1fbf9f689cc64b6cb036711c2a6fb
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2025-04-12 20:25:44 -0400

    feat: add discription in index.html

--- src/data/midiaGallery.ts
+++ src/data/midiaGallery.ts
@@ -1,5 +1,5 @@
 export const midiaGallery = [
-  "https://www.instagram.com/reel/DA6hO-XPsC_",
+  "https://www.instagram.com/reel/XYZ12345678",
   "https://www.instagram.com/reel/XYZ12345678",
   "https://www.instagram.com/reel/XYZ12345678",
   ];
